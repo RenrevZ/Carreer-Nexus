8f64bfcbeff723b8e4e6cce44db426c7290ca48e
Harvy Gascon <harvy275@gmail.com>
2023-03-16 21:52:06 +0800

    change im heart of developer

--- src/components/CreateJob.vue
+++ src/components/CreateJob.vue
@@ -15,7 +15,7 @@
         </div>
 
         <div class="relative z-0 w-full mb-6 group">
-            <input type="text" name="floating_email" id="floating_email" v-model="JobDescription" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            <input type="text" name="floating_email" id="floating_email" v-model="JobDescription"  class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
             <label for="floating_email" class="peer-focus:font-medium absolute left-0 text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                 Job Description
             </label>
@@ -32,11 +32,22 @@
         <h6>Job Highlights</h6>
         <div class="flex flex-col items-start justify-start" ref="JobHighlighDiv">
             <div class="relative z-0 w-full mb-6 group">
-                <input type="text" name="floating_email" id="floating_email" v-model="JobHighlight" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                <input type="text" name="floating_email" id="floating_email" v-model="JobHighlight" @keyup.enter="addItem" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                 <label for="floating_email" class="peer-focus:font-medium absolute left-0 text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                     Job Highlights
                 </label>
             </div>
+
+            <div v-if="Jharray">
+                <div v-for="highlight in limitHiglight" :key="highlight">
+                    <ul>
+                        <li>
+                            {{ highlight }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+           
             <i class="fa-solid fa-circle-plus text-3xl text-teal-500 cursor-pointer" @click="addJobHiglights"></i>
         </div>
     </form>
@@ -46,50 +57,26 @@
 </template>
 
 <script>
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 export default {
     setup(){
         const JobHighlighDiv = ref(null);
-        
-        onMounted(() => {
-            console.log(JobHighlighDiv)
+        let Jharray = ref([]);
+        const JobHighlight = ref('')
+       
+        const limitHiglight = computed(() => {
+            return Jharray.value.slice(0,5)
         })
-        // const addJobHiglights = () => {
-        //     const addJobHiglightField =`<div class="relative z-0 w-full mb-6 group">
-        //     <input type="text" name="floating_email" id="floating_email" v-model="JobHighlight" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-        //     <label for="floating_email" class="peer-focus:font-medium absolute left-0 text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-        //         Job Highlights
-        //     </label>
-        //     </div>
-        //     `
-        //     JobHighlighDiv.value.innerHTML.prepend(addJobHiglightField)
-        // }
-        const addJobHiglights = () => {
-            const newJobHighlightField = document.createElement('div');
-            newJobHighlightField.className = 'relative z-0 w-full mb-6 group';
 
-            const inputField = document.createElement('input');
-            inputField.type = 'text';
-            inputField.name = 'floating_email';
-            inputField.id = 'floating_email';
-            inputField.className = 'block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer';
-            inputField.placeholder = '';
-            inputField.required = true;
-            inputField.setAttribute('v-model', 'JobHighlight');
-
-            const label = document.createElement('label');
-            label.htmlFor = 'floating_email';
-            label.className = 'peer-focus:font-medium absolute left-0 text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6';
-            label.innerText = 'Job Highlights';
-
-            newJobHighlightField.appendChild(inputField);
-            newJobHighlightField.appendChild(label);
-
-            JobHighlighDiv.value.prepend(newJobHighlightField);
+        const addItem = () => {
+            if(JobHighlight.value != ''){
+                Jharray.value.push(JobHighlight.value)
+                JobHighlight.value = ''
+            }
         }
 
-
-         return {addJobHiglights,JobHighlighDiv}
+        console.log(limitHiglight)
+         return {addJobHiglights,JobHighlighDiv,JobHighlight,addItem,Jharray,limitHiglight}
     }
 }
 </script>
